55c329d996ccda1d673a13c479fea48b1b59691f
Artyom Palevich <hello@apalevich.ru>
2022-08-30 01:16:04 +0300

    Create component

---
+++ components/Joke.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="joke">
+    {{ joke }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Joke',
+    props: ['joke', 'id']
+}
+</script>
+
+<style>
+    .joke {
+        padding: 1rem;
+        margin: 1rem 0;
+        border: 1px dotted #ccc;
+    }
+</style>
