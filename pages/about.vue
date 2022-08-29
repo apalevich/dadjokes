b86340827e8d667b0351bc723e65ef5d437f0171
Artyom Palevich <hello@apalevich.ru>
2022-08-29 14:33:28 +0300

    Cosmetics changes

--- pages/about.vue
+++ pages/about.vue
@@ -1,30 +1,12 @@
 <template>
   <div>
-    <h1>About the DadJokes</h1>
-    <p>This is an app that display corny dad jokes</p>
-    <hr>
-    <p>
-      This page is rendered on the <strong>{{ rendering }}</strong>
-    </p>
-    <p v-if="rendering === 'server'">
-      First load or hard refresh is done on server side.
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>Refresh the page for server side rendering.</li>
-      <li>Click the links to see client side rendering.</li>
-    </ul>
-    <a href="/about">Links</a>
+    <h2>About the DadJokes</h2>
+    <p>This is an app that displays corny dad jokes</p>
   </div>
 </template>
 
 <script>
 export default {
-    asyncData() {
-        return {
-        rendering: process.server ? 'server' : 'client'
-        }
-    },
     head() {
         return {
             title: 'About the DadJokes',
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial/>
+  <h2>Best place for corny dad jokes</h2>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+    head() {
+      return {
+          title: 'About the DadJokes',
+          meta: [
+              {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Best place for corny dad jokes',
+              }
+          ],
+      }
+  }
 }
 </script>
