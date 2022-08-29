b77555aeeaef8d608ee9ea311c798d208ade490d
Artyom Palevich <hello@apalevich.ru>
2022-08-30 01:12:42 +0300

    Fetch jokes from API

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -3,7 +3,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config)
+
+            this.jokes = res.data.results;
+        } catch (e) {
+            alert('Sorry we have a problem fetching jokes from the API')
+            console.error(e);
+        }
+    },
     head() {
         return {
             title: 'Dad Jokes',
